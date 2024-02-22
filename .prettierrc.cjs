38438fd1559a56c11c6799bb5fd9459e7edb5af6
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2024-02-22 01:26:33 -0500

    chore(prettier): add config file

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+
+  plugins: [
+    '@prettier/plugin-xml'
+  ],
+
+  xmlWhitespaceSensitivity: 'ignore',
+}
